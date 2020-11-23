35f8311ca507022688669f1c84f7a474739a52c1
Kento-Ishizaki <kentoron1115@gmail.com>
2020-11-23 09:01:55 +0900

    Chore: ビルドエラー解消

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
-import 'dart:async';
-
 import 'package:bz_quiz/components/splashes/splash.dart';
 import 'package:bz_quiz/providers/quiz_form_provider.dart';
 import 'package:bz_quiz/providers/quiz_provider.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  Crashlytics.instance.enableInDevMode = true;
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
-  runZoned(() {
-    runApp(MyApp());
-  }, onError: Crashlytics.instance.recordError);
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
